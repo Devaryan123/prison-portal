ec525ad749006446a9748324325723ba5bed662d
Devaryan123 <123658511+Devaryan123@users.noreply.github.com>
2025-09-12 12:56:49 +0530

    another commit

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
   return (
     <div>
       <div className="flex items-center mt-20 ">
-        <p className="text-3xl flex font-medium ml-8 text-blue-800 font-serif w-70 h-20 ">
+        <p className="text-3xl flex font-medium ml-8 text-blue-800 font-serif w-80 h-25 ">
           Your platform for prison reform & research
           <Lightbulb size={80} color="#c4c615" />
           
--- components/MiddlePage.tsx
+++ components/MiddlePage.tsx
@@ -5,7 +5,7 @@ import { Bookmark, Edit3, FileText, Send, Sparkles, Zap } from "lucide-react";
 export default function NewsLayout() {
   return (
     <div className="p-4">
-      <div className="max-w-5xl mx-auto md:mx-25  justify-between gap-8">
+      <div className="max-w-5xl mx-auto md:mx-30  justify-between gap-8">
         {/* Featured Section */}
         <div className="flex items center flex-col md:flex-row gap-12  ">
           <div>
